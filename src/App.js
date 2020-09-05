c92911ca7ed88a9d661bf1418c9097d66d0a41f8
AgileDesign2 <agiledesign2.0@gmail.com>
2020-09-05 05:59:10 -0400

    update base app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import BaseRouter from './Routes'
-import HomePage from './Pages/Homepage';
-import LoginPage from './Pages/LoginPage';
-import LogoutPage from './Pages/LogoutPage';
-import SignupPage from './Pages/SignupPage';
-import ResetPage from './Pages/ResetPage';
-import ResetConfirmPage from './Pages/ResetConfirmPage';
 
 function App() {
   return (
